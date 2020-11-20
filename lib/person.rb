68322fcfb3a420e827dba5c7cdd68ba47356c94c
calmpsycho333 <wrathking75@yahoo.com>
2020-11-20 15:13:17 -0500

    commit tree fba32b5e508ff38c7a0ceeb9f479f9243bc44c4e

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def name=(person)
+        @name = person
+    end
+    def name
+        @name 
+    end
+    def job=(singer)
+        @job = singer 
+    end
+    def job 
+        @job
+    end
+end
